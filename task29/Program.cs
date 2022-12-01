b800211f3dedf99e817137693a328df05ef67f66
Aleksei Svitoch <pndlm@yandex.ru>
2022-12-01 20:03:50 +0600

    task 29 complete

--- task29/Program.cs
+++ task29/Program.cs
@@ -2,3 +2,12 @@
 // предоставить возможность пользователю ввести числа и вывести их на экран.
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
+Console.Clear();
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Введите массив: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    array[i] = n;
+}
+Console.Write($"[{string.Join(", ", array)}]");
